610cd375ded7cd72f52cbd23868d6e2ca5653c58
Bastian Heist <bastian.heist@sandstorm.de>
2018-06-10 16:52:22 +0200

    TASK: Beispiel TreeSet

---
+++ programmierbeispiele/src/collections/sortierung/CollectionsSortDemo.java
@@ -0,0 +1,30 @@
+package collections.sortierung;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.TreeSet;
+
+public class CollectionsSortDemo {
+    public static void main(String[] args) {
+        Student klaus = new Student("Klaus", "Klausen", "123456");
+        Student peter = new Student("Peter", "Petersen", "456789");
+        Student helga = new Student("Helga", "Hansen", "234567");
+        Student fritz = new Student("Fritz", "Fritzlar", "789123");
+        Student paula = new Student("Paula", "Paulsen", "123789");
+
+        TreeSet<Student> set = new TreeSet<>();
+
+        set.add(klaus);
+        set.add(peter);
+        set.add(helga);
+        set.add(fritz);
+        set.add(paula);
+
+        Iterator<Student> it = set.iterator();
+
+        while(it.hasNext()) {
+            Student s = it.next();
+            s.print();
+        }
+    }
+}
---
+++ programmierbeispiele/src/collections/sortierung/Student.java
@@ -0,0 +1,33 @@
+package collections.sortierung;
+
+public class Student implements Comparable<Student> {
+    private String vorname;
+    private String nachname;
+    private String matrikelNr;
+
+    public Student(String vorname, String nachname, String matrikelNr) {
+        this.vorname = vorname;
+        this.nachname = nachname;
+        this.matrikelNr = matrikelNr;
+    }
+
+    public int compareTo(Student s) {
+        return Integer.parseInt(matrikelNr) - Integer.parseInt(s.getMatrikelNr());
+    }
+
+    public void print() {
+        System.out.println(vorname + " " + nachname + " " + matrikelNr);
+    }
+
+    public String getVorname() {
+        return vorname;
+    }
+
+    public String getNachname() {
+        return nachname;
+    }
+
+    public String getMatrikelNr() {
+        return matrikelNr;
+    }
+}
