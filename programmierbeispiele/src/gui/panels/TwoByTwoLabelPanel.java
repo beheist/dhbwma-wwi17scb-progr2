5bac8ccd40c243276cba3f520049c6a3eb6df539
Bastian Heist <bastian.heist@sandstorm.de>
2018-06-28 13:41:18 +0200

    TASK: Improve code example for panel layout

--- programmierbeispiele/src/gui/panels/TwoByTwoLabelPanel.java
+++ programmierbeispiele/src/gui/panels/TwoByTwoLabelPanel.java
@@ -6,9 +6,9 @@ import java.awt.*;
 public class TwoByTwoLabelPanel extends JPanel {
     public TwoByTwoLabelPanel(String text1, String text2, String text3, String text4){
         super(new GridLayout(2,2));
-        this.add(new JLabel(text1));
-        this.add(new JLabel(text2));
-        this.add(new JLabel(text3));
-        this.add(new JLabel(text4));
+        this.add(new JLabel(text1, SwingConstants.CENTER));
+        this.add(new JLabel(text2, SwingConstants.CENTER));
+        this.add(new JLabel(text3, SwingConstants.CENTER));
+        this.add(new JLabel(text4, SwingConstants.CENTER));
     }
 }
